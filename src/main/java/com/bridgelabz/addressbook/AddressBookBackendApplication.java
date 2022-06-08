96734fed6e3ab953918035cd46d7b3904e2c5f44
Archanagowda23 <archanar249@gmail.com>
2022-06-08 22:02:49 +0530

    Created Addressbook app project and added welcome message

--- src/main/java/com/bridgelabz/addressbook/AddressBookBackendApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressBookBackendApplication {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to Addressbook App");
         SpringApplication.run(AddressBookBackendApplication.class, args);
     }
 }
