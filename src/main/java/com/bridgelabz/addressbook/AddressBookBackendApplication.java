c65ccfdba6519917325f329b633b3e223174cb0f
Archanagowda23 <archanar249@gmail.com>
2022-06-08 21:40:59 +0530

    Added required files

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBookBackendApplication.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookBackendApplication.class, args);
+    }
+}
